9e4e00238b415b45d137b702fe012a978a3505a2
NazRinRus <NazRinRus@gmail.com>
2025-04-25 12:14:53 +0300

    modify FileModel add get_filenamt and get_filepath

--- fastapi-app/modules/models.py
+++ fastapi-app/modules/models.py
@@ -2,14 +2,16 @@
 class FileModel:
     def __init__(
             self,
-            file_path: str,
             file_name: str,
             file_content: str,
             file_updated_at: str = None
             ):
-        self.file_path = file_path,
         self.file_name = file_name,
         self.file_content = file_content,
         self.file_updatad_at = file_updated_at
+    def get_filename(self):
+        return self.file_name.split('/')[-1]
+    def get_filepath(self):
+        return '/'.join(self.file_name.split('/')[:-1])
     def info(self):
-        return (self.file_name, self.file_updatad_at)
+        return (self.get_filename(), self.file_updatad_at)
